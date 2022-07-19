b5b9f9463570a5e83508d5068def51c488077df0
abcb2 <kouji.wtnb@gmail.com>
2022-07-19 23:23:49 +0900

    implement 128

--- murmur128.go
+++ murmur128.go
@@ -72,18 +72,93 @@ func (s *sum128) Write(data []byte) (n int, err error) {
 
 	switch dataLen & 15 {
 	case 15:
+		k2 ^= uint64(tail[14]) << 48
+		fallthrough
 	case 14:
+		k2 ^= uint64(tail[13]) << 40
+		fallthrough
 	case 13:
+		k2 ^= uint64(tail[12]) << 32
+		fallthrough
 	case 12:
+		k2 ^= uint64(tail[11]) << 24
+		fallthrough
 	case 11:
+		k2 ^= uint64(tail[10]) << 16
+		fallthrough
+	case 10:
+		k2 ^= uint64(tail[9]) << 8
+		fallthrough
+	case 9:
+		k2 ^= uint64(tail[8]) << 0
+
+		k2 *= c2_128
+		k2 = bits.RotateLeft64(k2, 33)
+		k2 *= c1_128
+		s.h2 ^= k2
+		fallthrough
+	case 8:
+		k1 ^= uint64(tail[7]) << 56
+		fallthrough
+	case 7:
+		k1 ^= uint64(tail[6]) << 48
+		fallthrough
+	case 6:
+		k1 ^= uint64(tail[5]) << 40
+		fallthrough
+	case 5:
+		k1 ^= uint64(tail[4]) << 32
+		fallthrough
+	case 4:
+		k1 ^= uint64(tail[3]) << 24
+		fallthrough
+	case 3:
+		k1 ^= uint64(tail[2]) << 16
+		fallthrough
+	case 2:
+		k1 ^= uint64(tail[1]) << 8
+		fallthrough
+	case 1:
+		k1 ^= uint64(tail[0]) << 0
+		k1 *= c1_128
+		k1 = bits.RotateLeft64(k1, 31)
+		k1 *= c2_128
+		s.h1 ^= k1
 	}
+
+	s.h1 ^= uint64(dataLen)
+	s.h2 ^= uint64(dataLen)
+
+	s.h1 += s.h2
+	s.h2 += s.h1
+
+	s.h1 = fmix64(s.h1)
+	s.h2 = fmix64(s.h2)
+
+	s.h1 += s.h2
+	s.h2 += s.h1
 
 	return dataLen, nil
 }
 
+func fmix64(k uint64) uint64 {
+	k ^= k >> 33
+	k *= 0xff51afd7ed558ccd
+	k ^= k >> 33
+	k *= 0xc4ceb9fe1a85ec53
+	k ^= k >> 33
+	return k
+}
+
 func (s *sum128) Sum(b []byte) []byte {
-	//TODO implement me
-	panic("implement me")
+	h1, h2 := s.h1, s.h2
+	return append(b,
+		byte(h1>>56), byte(h1>>48), byte(h1>>40), byte(h1>>32),
+		byte(h1>>24), byte(h1>>16), byte(h1>>8), byte(h1),
+
+		byte(h2>>56), byte(h2>>48), byte(h2>>40), byte(h2>>32),
+		byte(h2>>24), byte(h2>>16), byte(h2>>8), byte(h2),
+	)
 }
 
 func (s *sum128) Size() int {
